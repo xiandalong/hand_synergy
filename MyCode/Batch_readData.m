947b1132067255252eab5e2d27339912994af145
xiandalong <kelvinlong8897@gmail.com>
2014-12-09 15:17:34 -0500

    added PCA analysis and scatter plots

--- MyCode/Batch_readData.m
+++ MyCode/Batch_readData.m
@@ -1,4 +1,4 @@
-tic
+
 
 %% read all the data from the *.csv files and convert into joint angles
 xls_path = 'S:\Xianda\Dropbox\Haptics research\Data and docs\recording_list.xlsx';
@@ -15,7 +15,6 @@ Right_JA_col_index = 7;
 
 for j=1:size(table,1)
     
-    tic
     
     % get path for one file
     csv_path = fullfile(data_dir,table.filename{j});
@@ -47,10 +46,64 @@ for j=1:size(table,1)
     Data_array{j,6} = joint_angles_LeftHand;
     Data_array{j,7} = joint_angles_RightHand;
 
-    toc
+    
 end
 
+%% PCA and E-distance calculation
+% put all the data into a 2D matrix for PCA
 
+% Left Hand
+num_trials = size(Data_array,1);
+raw_data_Left = zeros(num_trials,20);
+for j=1:size(Data_array,1)
+    raw_data_Left(j,:)= ( mean(Data_array{j,Left_JA_col_index},2))';
+end
 
+% Right Hand
+num_trials = size(Data_array,1);
+raw_data_Right = zeros(num_trials,20);
+for j=1:size(Data_array,1)
+    raw_data_Right(j,:)= ( mean(Data_array{j,Right_JA_col_index},2))';
+end
 
-toc
+[coeff,score,latent,tsquared,explained,mu] = pca([raw_data_Left;raw_data_Right]);
+
+% show two clusters from left and right hand
+figure
+scatter3(score(1:60,1),score(1:60,2),score(1:60,3));
+hold on
+scatter3(score(61:120,1),score(61:120,2),score(61:120,3),'red');
+legend('Left Hand','Right Hand');
+
+% show the detailed classification
+figure
+size = 50; 
+% 1. cone
+scatter3(score(1:5,1),score(1:5,2),score(1:5,3),size,[ 1 1 0 ],'o');%  async
+hold on
+scatter3(score(6:10,1),score(6:10,2),score(6:10,3),size,[ 1 1 0],'*');%  sync
+
+% 2. cylinder 
+scatter3(score(11:15,1),score(11:15,2),score(11:15,3),size,[  1 0 1],'o');% async
+scatter3(score(16:20,1),score(16:20,2),score(16:20,3),size,[  1 0 1],'*');% sync
+
+% 3. drum
+scatter3(score(21:25,1),score(21:25,2),score(21:25,3),size,[ 0 1 1],'o');% async
+scatter3(score(26:30,1),score(26:30,2),score(26:30,3),size,[ 0 1 1],'*');% sync
+
+% 4. mouse
+scatter3(score(31:35,1),score(31:35,2),score(31:35,3),size,[ 1 0 0],'o');% async
+scatter3(score(36:40,1),score(36:40,2),score(36:40,3),size,[ 1 0 0],'*');% sync
+
+% 5. papercup
+scatter3(score(41:45,1),score(41:45,2),score(41:45,3),size,[ 0 1 0],'o');% async
+scatter3(score(46:50,1),score(46:50,2),score(46:50,3),size,[ 0 1 0],'*');% sync
+
+% 6. pen
+scatter3(score(51:55,1),score(51:55,2),score(51:55,3),size,[ 0 0 1],'o');% async
+scatter3(score(56:60,1),score(56:60,2),score(56:60,3),size,[ 0 0 1],'*');% sync
+
+% right hand
+scatter3(score(61:120,1),score(61:120,2),score(61:120,3),size,'s');
+legend('cone async','cone sync','cylinder async','cylinder sync','drum async','drum sync',...
+    'mouse async','mouse sync','papercup async','papercup sync','pen async','pen sync','RIGHT HAND');
